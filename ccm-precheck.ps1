4bda8c25d952fa17aaca7d53ce9bab4dd08f657d
KevinHo317 <hky7046@gmail.com>
2020-06-12 17:24:39 +0800

    Update ccm-precheck.ps1

--- ccm-precheck.ps1
+++ ccm-precheck.ps1
@@ -1,1 +1,10 @@
-
+$CertList = (Get-ChildItem -Path Cert:\LocalMachine\My | where{ $_.EnhancedKeyUsageList -match "Client Authentication" -and {$_.IssuerName -match "CN=Enterprise Sub CA"}}).NotAfter
+    if ($CertList -eq $null){Write-Host "No Client Authentication Certificate found"}else{
+    $Today = get-date
+foreach($certificate in $CertList){
+    If ($certificate -gt $today.AddDays(365)) {
+        $result = "True"
+    }
+    }
+  }
+  Write-Host $result
